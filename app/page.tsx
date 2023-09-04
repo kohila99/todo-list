24c453edf13582957938502db36d733eb8229fb8
Kohila A <ykohila@gmail.com>
2023-09-04 18:19:43 +0530

    make the update and cancel buttons

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,47 @@ const App = () => {
     <div className="w-1/3 m-auto mt-10">
       <div className="flex justify-between">
         <input type="text" className="border border-black" />
-        <button className="bg-sky-500 text-white px-2 rounded">Add</button>
+        <div>
+          <button className="bg-green-500 mr-2 text-white px-2 rounded">
+            Update
+          </button>
+          <button className="bg-red-500 text-white px-2 rounded">Cancel</button>
+
+          {/* <button className="bg-blue-500 text-white px-2 rounded">Add</button> */}
+        </div>
       </div>
       <ul className="mt-5">
         <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
           Arun
           <div className="flex">
-            <FaPencil className="mr-3" />
-            <FaTrashCan />
+            <button>
+              <FaPencil className="mr-3" />
+            </button>
+            <button>
+              <FaTrashCan />
+            </button>
           </div>
         </li>
         <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
           Kohi
           <div className="flex">
-            <FaPencil className="mr-3" />
-            <FaTrashCan />
+            <button>
+              <FaPencil className="mr-3" />
+            </button>
+            <button>
+              <FaTrashCan />
+            </button>
           </div>
         </li>
         <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
           Ayron
           <div className="flex">
-            <FaPencil className="mr-3" />
-            <FaTrashCan />
+            <button>
+              <FaPencil className="mr-3" />
+            </button>
+            <button>
+              <FaTrashCan />
+            </button>
           </div>
         </li>
       </ul>
