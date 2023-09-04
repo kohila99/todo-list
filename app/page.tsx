8a54bea2819baa2489c07ed202bbb94147c059f4
Kohila A <ykohila@gmail.com>
2023-09-04 23:14:24 +0530

    deleted App component code

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,5 @@
-import { FaTrashCan, FaPencil } from "react-icons/fa6";
-
 const App = () => {
-  return (
-    <div className="w-1/3 m-auto mt-10">
-      <div className="flex justify-between">
-        <input type="text" className="border border-black" />
-        <div>
-          <button className="bg-blue-500 mr-2 text-white px-2 rounded">
-            Update
-          </button>
-          <button className="bg-red-500 text-white px-2 rounded">Cancel</button>
-
-          {/* <button className="bg-blue-500 text-white px-2 rounded">Add</button> */}
-        </div>
-      </div>
-      <ul className="mt-5">
-        <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
-          Arun
-          <div className="flex">
-            <button>
-              <FaPencil className="mr-3 text-blue-500" />
-            </button>
-            <button>
-              <FaTrashCan className=" text-red-500" />
-            </button>
-          </div>
-        </li>
-        <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
-          Kohi
-          <div className="flex">
-            <button>
-              <FaPencil className="mr-3  text-blue-500" />
-            </button>
-            <button>
-              <FaTrashCan className=" text-red-500" />
-            </button>
-          </div>
-        </li>
-        <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
-          Ayron
-          <div className="flex">
-            <button>
-              <FaPencil className="mr-3 text-blue-500" />
-            </button>
-            <button>
-              <FaTrashCan className=" text-red-500" />
-            </button>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div className="w-1/3 m-auto mt-10"></div>;
 };
 
 export default App;
