3592845c20217e051bbbcc486301c060bc911d30
Kohila A <ykohila@gmail.com>
2023-09-05 17:34:43 +0530

    create ul and li

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 "use client";
 
+import { useState } from "react";
+
 const App = () => {
-  alert("hi");
+  const [state, setState] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setState(e.target.value);
+  };
+  const handleClick = () => {
+    alert(state);
+  };
   return (
-    <div className=" mt-5">
-      <input type="text" className="border border-black" />
-      <button className=" bg-blue-500">Add</button>
+    <div className=" mt-5 m-auto w-1/3">
+      <input
+        type="text"
+        className="border border-black"
+        onChange={handleChange}
+      />
+      <button className=" bg-blue-500" onClick={handleClick}>
+        Add
+      </button>
+      <ul>
+        <li>Ayron</li>
+        <li>Niami</li>
+        <li>Kohi</li>
+      </ul>
     </div>
   );
 };
