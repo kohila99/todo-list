1fbcabd918aff7cfddd78dd9db4d37ff53ea6526
Kohila A <ykohila@gmail.com>
2023-09-07 23:23:05 +0530

    create li using array state

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,44 @@
+"use client";
+
+import { useState } from "react";
 import { FaTrashCan, FaPencil } from "react-icons/fa6";
 
 const App = () => {
+  const [names, setNames] = useState(["niami", "Kohi", "arun"]);
+  const exam = () => {
+    return "hi";
+  };
+
   return (
     <div className="w-1/3 m-auto mt-10">
       <div className="flex justify-between">
         <input type="text" className="border border-black" />
         <div>
-          <button className="bg-blue-500 mr-2 text-white px-2 rounded">
+          {/* <button className="bg-blue-500 mr-2 text-white px-2 rounded">
             Update
           </button>
-          <button className="bg-red-500 text-white px-2 rounded">Cancel</button>
+          <button className="bg-red-500 text-white px-2 rounded">Cancel</button> */}
 
-          {/* <button className="bg-blue-500 text-white px-2 rounded">Add</button> */}
+          <button className="bg-blue-500 text-white px-2 rounded">Add</button>
         </div>
       </div>
       <ul className="mt-5">
-        <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
-          Arun
-          <div className="flex">
-            <button>
-              <FaPencil className="mr-3 text-blue-500" />
-            </button>
-            <button>
-              <FaTrashCan className=" text-red-500" />
-            </button>
-          </div>
-        </li>
-        <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
-          Kohi
-          <div className="flex">
-            <button>
-              <FaPencil className="mr-3  text-blue-500" />
-            </button>
-            <button>
-              <FaTrashCan className=" text-red-500" />
-            </button>
-          </div>
-        </li>
-        <li className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md">
-          Ayron
-          <div className="flex">
-            <button>
-              <FaPencil className="mr-3 text-blue-500" />
-            </button>
-            <button>
-              <FaTrashCan className=" text-red-500" />
-            </button>
-          </div>
-        </li>
+        {names.map((value, index) => (
+          <li
+            className="border flex justify-between border-black mb-3 p-2 shadow-gray-600 shadow-md capitalize"
+            key={index}
+          >
+            {value}
+            <div className="flex">
+              <button>
+                <FaPencil className="mr-3 text-blue-500" />
+              </button>
+              <button>
+                <FaTrashCan className=" text-red-500" />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
